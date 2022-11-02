d27af346a60dc01ac612a300def21c172798d17e
seokzin <seokzin95@gmail.com>
2022-11-02 10:50:24 +0900

    feat: styled-components 설치

---
+++ src/components/Button/Button.styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Layout = styled.button`
+  background-color: #fff;
+  border-radius: 4px;
+`;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import { Layout } from './Button.styled';
+
 export interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
@@ -31,21 +33,10 @@ const Button = ({
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary
-    ? 'storybook-button--primary'
-    : 'storybook-button--secondary';
-
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(
-        ' '
-      )}
-      style={{ backgroundColor }}
-      {...props}
-    >
+    <Layout type="button" style={{ backgroundColor }} {...props}>
       {label}
-    </button>
+    </Layout>
   );
 };
 
