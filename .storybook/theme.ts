da2747bb4dd0757ada15bfc1f7e294a2bee52946
seokzin <seokzin95@gmail.com>
2022-11-03 11:14:54 +0900

    refac: 테마 설정 정렬

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -1,10 +1,16 @@
 import { create } from '@storybook/theming'
 
 export default create({
+  // Theme
   base: 'light',
-  brandTitle: 'Space',
-  brandImage: '/images/logo.svg',
-  fontBase: '"Pretendard Variable", sans-serif',
   colorPrimary: '#4859ED',
   colorSecondary: '#8F45ED',
+
+  // Typography
+  fontBase: '"Pretendard Variable", sans-serif',
+  fontCode: 'monospace',
+
+  // Brand
+  brandTitle: 'Space',
+  brandImage: '/images/logo.svg',
 })
