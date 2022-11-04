54554bc647fc9fcefedb0e4e9678e1fb130a7f85
seokzin <seokzin95@gmail.com>
2022-11-04 16:58:25 +0900

    feat: 스토리북 일부 애드온 제거

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,10 +6,8 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
     '@storybook/addon-a11y',
     'storybook-dark-mode',
-    'addon-screen-reader',
   ],
   framework: '@storybook/react',
   typescript: {
