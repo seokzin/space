baf8bfc86c199ec0f9f871444215e799abb354fc
seokzin <seokzin95@gmail.com>
2022-11-04 09:47:02 +0900

    feat: 스토리북 viewport 애드온 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,3 +1,4 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { addDecorator } from '@storybook/react'
 import React from 'react'
 
@@ -9,3 +10,17 @@ addDecorator((Story) => (
     <Story />
   </>
 ))
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    expanded: true,
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+}
