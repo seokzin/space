087e1e8d454fbf4a0e5a2acd3cbbebfd74141528
seokzin <seokzin95@gmail.com>
2022-11-30 22:13:35 +0900

    fix: 스토리북 빌드 에러 해결

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import pkg from './package.json' assert { type: 'json' }
+import pkg from './package.json'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
 const external = ['react', 'react-dom', 'styled-components']
--- src/components/Accordion copy/Accordion.tsx
+++
@@ -1,7 +0,0 @@
-import { Layout } from './Accordion.styled'
-
-const Accordion = () => {
-  return <Layout>Accordion</Layout>
-}
-
-export default Accordion
